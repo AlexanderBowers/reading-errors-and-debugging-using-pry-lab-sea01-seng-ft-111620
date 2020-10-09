66bac330b381f50c41cc4a3a6e4e1e09d98dcf9b
Alexander Bowers <7319abowers@gmail.com>
2020-10-09 18:43:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,9 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      puts "HAM HAM HAM IN MY TUMMY"
-  elsif num = 2
+  elsif num == 2
      puts "YUM YUM MUNCH MUNCH MUNCH"
   end
 end
