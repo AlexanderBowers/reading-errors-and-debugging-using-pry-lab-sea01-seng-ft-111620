167667561bf2da27d4e1773274c6229a764f7460
Alexander Bowers <7319abowers@gmail.com>
2020-10-09 18:54:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
-  if string[0] == "s"
-    "s" + string
-  else
-    string
-  end
-end
+#def snake_it_up(string)
+#  if string[0] == "s"
+  #  "s" + string
+#  else
+#    string
+#  end
+#end
 
 
 
