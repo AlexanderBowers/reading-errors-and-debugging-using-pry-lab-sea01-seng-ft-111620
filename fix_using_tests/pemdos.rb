1aa15d90d898031aeae5f3d767e8566904543086
Alexander Bowers <7319abowers@gmail.com>
2020-10-09 18:52:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  counter = 0
-  while count < 10
   if string[0] == "s"
-    10 * "s" + string
+    "s" + string
   else
     string
   end
